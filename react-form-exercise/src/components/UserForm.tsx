83e5393db432462196ee349e7324a6a736adb21a
Majito Brinkitos <mcvef2013@gmail.com>
2025-05-22 10:37:01 -0700

    form

--- react-form-exercise/src/components/UserForm.tsx
+++ react-form-exercise/src/components/UserForm.tsx
@@ -1,26 +1,72 @@
+
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+    firstname: string,
+    lastname: string,
+    age: number,
+    chicken: boolean,
+    beef: boolean,
+    vegetables: boolean,
+    dessert: boolean,
+    pork: boolean
+}
+
 const UserForm = () => {
+
+    const {
+        register,
+        handleSubmit,
+    } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) =>{
+        const favoriteFoods = Object.keys(data)
+        .filter((key) => data[key as keyof FormData] === true)
+        .join(", ");
+
+        alert(`Welcome ${data.firstname} ${data.lastname}. You are ${data.age} years old. And your favorite foods are ${favoriteFoods}`);
+    };
+
     return (
         <div className="userForm">
             <h1 className="font-h1 flex justify-center">User Form</h1>
-                <form className=" border-1 border-black grid grid-cols-6 content-around gap-4">
+                <form onSubmit={handleSubmit(onSubmit)} className=" border-1 border-black grid grid-cols-6 content-around gap-4">
                     {/*First Name */}
                     <div className="p-2 col-start-3">
                         <label htmlFor="firstname">
-                            First name: <input type="text" id="firstname" name="firstname" className="bg-cyan-500 text-white" />
+                            First name: <input 
+                            {
+                            ...register("firstname", {
+                                required: "First name is required"
+                            })    
+                            } 
+                            type="text" id="firstname" name="firstname" className="bg-cyan-500 text-white" />
                         </label>
                     </div>
 
                     {/*Last Name */}
                     <div className="p-2 col-start-3 col-end-4">
                         <label htmlFor="lastname">
-                            Last name: <input type="text" id="lastname" name="lastname" className="bg-cyan-500 text-white" />
+                            Last name: <input 
+                            {
+                                ...register("lastname", {
+                                    required: "Last name is required"
+                                })
+                            }
+                            type="text" id="lastname" name="lastname" className="bg-cyan-500 text-white" />
                         </label>
                     </div>
 
                     {/*Age */}
                     <div className="p-2 col-start-3 col-end-4">
                         <label htmlFor="age">
-                            Age: <input type="number" id="age" name="age" className="bg-cyan-500 text-white" />
+                            Age: <input 
+                            {
+                                ...register("age", {
+                                    required: "Age is required",
+                                })
+                            }
+                            type="number" id="age" name="age" className="bg-cyan-500 text-white" />
                         </label>
                     </div>
 
@@ -28,32 +74,63 @@ const UserForm = () => {
                     <div className="p-2 col-start-3 col-end-6">
                         <div className="inline-block">
                             <div className="flex">
-                                <label htmlFor="local">
-                                Local Food <input type="checkbox" id="local" name="local" className="bg-cyan-500 text-white flex"/>
+                                <label htmlFor="chicken">
+                                Chicken <input 
+                                {
+                                    ...register("chicken")
+                                }
+                                type="checkbox" id="chicken" name="chicken" className="bg-cyan-500 text-white flex"/>
                                 </label>
                             </div>
 
                             <div className="flex">
-                                <label htmlFor="home">
-                                Home-made food <input type="checkbox" id="home" name="home" className="bg-cyan-500 text-white flex"/>
+                                <label htmlFor="beef">
+                                Beef <input
+                                {
+                                    ...register("beef")
+                                }
+                                type="checkbox" id="beef" name="beef" className="bg-cyan-500 text-white flex"/>
                                 </label>
                             </div>
                             
                             <div className="flex">
-                                <label htmlFor="inter">
-                                International Food <input type="checkbox" id="inter" name="inter" className="bg-cyan-500 text-white flex"/>
+                                <label htmlFor="vegetables">
+                                Vegetables <input 
+                                {
+                                    ...register("vegetables")
+                                }
+                                type="checkbox" id="vegetables" name="vegetables" className="bg-cyan-500 text-white flex"/>
                                 </label>
                             </div>
                             
                             <div className="flex">
-                                <label htmlFor="fast">
-                                Fast Food <input type="checkbox" id="fast" name="fast" className="bg-cyan-500 text-white flex"/>
+                                <label htmlFor="dessert">
+                                Dessert <input 
+                                {
+                                    ...register("dessert")
+                                }
+                                type="checkbox" id="dessert" name="dessert" className="bg-cyan-500 text-white flex"/>
+                                </label>
+                            </div>
+
+                            <div className="flex">
+                                <label htmlFor="pork">
+                                Pork <input
+                                {
+                                    ...register("pork")
+                                }
+                                type="checkbox" id="pork" name="pork" className="bg-cyan-500 text-white flex"/>
                                 </label>
                             </div>
                 
 
                         </div>
                     </div>
+
+                    {/*Display User Button */}
+                    <button type="submit" className="bg-[orange] text-black font-bold rounded-lg w-25 h-20 items-center p-5 flex">
+                        Display User
+                    </button>
                 </form>
         </div>
     );
